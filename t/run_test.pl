071dfa79140bb8865f4f479abe1701fe82ca857c
Niklas Terhoeven <niklaus.terhoeven@stud-mail.uni-wuerzburg.de>
2015-06-18 11:04:11 +0200

    testing, if script produces output file

--- t/run_test.pl
+++ t/run_test.pl
@@ -4,11 +4,19 @@ use warnings;
 use Test::More;
 
 my$script='../blast2bed';
-my$input='./test_input.bls';
-my$expected='./expected_output.bed';
+my$input='test_input.bls';
+my$expected='expected_output.bed';
+my$result='test_input.bed';
 
-is(-e $script,1,'script found');
-is(-e $input,1,'input found');
-is(-e $expected,1,'expected output found');
+ok(-e $script,'script found');
+ok(-e $input,'input found');
+ok(-e $expected,'expected output found');
+
+print STDERR "removing previous test results file $result if exists\n";
+system("rm $result") if -e $result;
+
+print STDERR "running $script $input\n";
+system("$script $input");
+ok(-e $result, 'script produced output');
 
 done_testing();
