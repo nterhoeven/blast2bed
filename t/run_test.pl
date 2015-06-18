628e45459dc1a77bb15b66ad6c39fa529b3d5b06
Niklas Terhoeven <niklaus.terhoeven@stud-mail.uni-wuerzburg.de>
2015-06-18 10:47:14 +0200

    test added: check for sample input and expected output files

--- t/run_test.pl
+++ t/run_test.pl
@@ -4,7 +4,11 @@ use warnings;
 use Test::More;
 
 my$script='../blast2bed';
+my$input='test_input.bls';
+my$expected='expected_output.bls';
 
 is(-e $script,1,'script found');
+is(-e $input,1,'input found');
+is(-e $expected,1,'expected output found');
 
 done_testing();
